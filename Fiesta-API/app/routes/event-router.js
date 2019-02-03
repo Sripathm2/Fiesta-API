7018427cac34feff74958bfad52c29554ae75883
Ethan Niu <niu.ethan@outlook.com>
2019-02-02 19:33:44 -0500

    Changed tabs to spaces.

--- Fiesta-API/app/routes/event-router.js
+++ Fiesta-API/app/routes/event-router.js
@@ -11,7 +11,7 @@ let eventRoutes = express.Router();
  * @apiGroup event
  *
  * @apiQuery (body) {String} searchRequest of the user.
- *						{String} budget of the user.
+ *                      {String} budget of the user.
  *
  * @apiSuccess {String} up to 100 results.
  * @apiError (RequestFormatError) 422 For missing data or invalid searchRequest or budget.
@@ -19,54 +19,54 @@ let eventRoutes = express.Router();
 **/
 
 eventRoutes.get('/event', (req, res) => {
-	
-	if (!req.query.searchRequest) {
-		return res.status(422).send({
-			errorType: 'RequestFormatError',
-			message: 'Must include searchRequest.',
-		});
-	}
-	
-	if (!req.query.budget) {
-		return res.status(422).send({
-			errorType: 'RequestFormatError',
-			message: 'Must include budget.',
-		});
-	}
-	
-	var searchRadius = 50;
+    
+    if (!req.query.searchRequest) {
+        return res.status(422).send({
+            errorType: 'RequestFormatError',
+            message: 'Must include searchRequest.',
+        });
+    }
+    
+    if (!req.query.budget) {
+        return res.status(422).send({
+            errorType: 'RequestFormatError',
+            message: 'Must include budget.',
+        });
+    }
+    
+    var searchRadius = 50;
 
-	axios.get('https://eventup.com/api/v3/search/place/', {
-		params: {
-			budget: req.query.budget,
-			radius: searchRadius,
-			search: req.query.searchRequest,
-		}
-	})
-	.then(function (response) {
-		var ven = response.data.venues;
-		var ret = [];
-		for (var i = 0; i < response.data.count; i++) {
-			if (i >= 100) {
-				break;
-			}
-			if (i in ven) {
-				if ('title' in ven[i]) {
-					ret.push(ven[i]['title']);
-					console.log(ven[i]['title']);
-				}
-			}
-		}
-		//console.log(ret);
-		res.send({
-			results: ret,
-		});
-	})
-	.catch(function (error) {
-		//console.log(error);
-		return res.status(500).send({
-			errorType: 'Internal Error',
-			message: error,
-		});
-	});
+    axios.get('https://eventup.com/api/v3/search/place/', {
+        params: {
+            budget: req.query.budget,
+            radius: searchRadius,
+            search: req.query.searchRequest,
+        }
+    })
+    .then(function (response) {
+        var ven = response.data.venues;
+        var ret = [];
+        for (var i = 0; i < response.data.count; i++) {
+            if (i >= 100) {
+                break;
+            }
+            if (i in ven) {
+                if ('title' in ven[i]) {
+                    ret.push(ven[i]['title']);
+                    console.log(ven[i]['title']);
+                }
+            }
+        }
+        //console.log(ret);
+        res.send({
+            results: ret,
+        });
+    })
+    .catch(function (error) {
+        //console.log(error);
+        return res.status(500).send({
+            errorType: 'Internal Error',
+            message: error,
+        });
+    });
 });
