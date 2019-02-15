5bf097468531a112c814b821ea7d07a2113d33f3
Sripath Mishra <mishra60@purdue.edu>
2019-02-15 14:51:57 +0000

    The update events route

--- app/routes/event-router.js
+++ app/routes/event-router.js
@@ -5,6 +5,8 @@ let nodemailer = require('nodemailer');
 let fs = require('fs');
 const connectionString = process.env.DB_URL;
 const Insert_event = 'INSERT INTO Events (owner, date, location, partySupplier, caterer, guests) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id';
+const Update_event = 'Update Users Set date = $1, location = $2, partySupplier = $3, caterer = $4, guests = $5 Where owner = $6 AND id = $7';
+const Select_event = 'Select * from Events where owner = $1 AND id = $2';
 
 // Instantiate router
 
@@ -115,7 +117,7 @@ eventRoutes.get('/event', (req, res) => {
             partySupplier: 'Party City',
             caterer: 'Chipotle',
             guests: [ 'Jane Doe', 'Scott Smith', 'George Washington' ]
-
+        }
  * @apiSuccess {String} eventID.
  * @apiError (RequestFormatError) 422 For missing parameter(s).
  * @apiError (Internal Error) 500+ Internal Error.
@@ -192,6 +194,102 @@ eventRoutes.post('/create', (req, res) => {
         return res.send({
             eventID: response.rows[0].id,
         });
+    });
+});
+
+/**
+ * @api {post} /update
+ * @apiName update
+ * @apiGroup event
+ *
+ * @apiParam (body) {String} date of event in format 'YYYY-MM-DD HH:mm:SS'
+ *                  {float[2]} array of floats containing longitude/latitude.
+ *                  {String} name of party supplier.
+ *                  {String} name of caterer.
+ *                  {String[]} array of guests invited.
+                    {String} id of the event
+ *
+ * @apiParamExample {JSON} Request Body Example
+ *      {
+            date: '2019-02-09 05:00:00',
+            location: [ 40.423540, -86.921740 ],
+            partySupplier: 'Party City',
+            caterer: 'Chipotle',
+            guests: [ 'Jane Doe', 'Scott Smith', 'George Washington' ]
+        }
+ * @apiSuccess {String} message: success.
+ * @apiError (RequestFormatError) 422 For missing parameter(s).
+ * @apiError (Internal Error) 500+ Internal Error.
+**/
+
+eventRoutes.post('/update', (req, res) => {
+
+    if (!req.body.userName) {
+        return res.status(422).send({
+            errorType: 'RequestFormatError',
+            message: 'Must include the owner userName.',
+        });
+    }
+
+    if (!req.body.id) {
+        return res.status(422).send({
+            errorType: 'RequestFormatError',
+            message: 'Must include the id.',
+        });
+    }
+
+    let event = {};
+    event.owner = req.body.userName;
+    event.id = req.body.id;
+
+    const pool = new Pool({
+        connectionString: connectionString,
+    });
+
+    pool.query(Select_event, [event.owner, event.id, ],  (err, response) => {
+
+        if(err){
+            pool.end();
+            return res.send({
+                errorType: 'InternalError',
+                message: err,
+            });
+        }
+
+        if(!response.rows[0]){
+            return res.status(422).send({
+                errorType: 'NoSuchEventError',
+                message: 'Incorrect owner or id.',
+            });
+        }
+
+        event.date = req.body.date? req.body.date : response.rows[0].date;
+        event.location = req.body.location? req.body.location: response.rows[0].location;
+        event.partySupplier = req.body.partySupplier? req.body.partySupplier: response.rows[0].partySupplier;
+        event.caterer = req.body.caterer?  req.body.caterer: response.rows[0].caterer;
+        event.guests = req.body.guests? req.body.guests: response.rows[0].guests;
+
+        const pool1 = new Pool({
+            connectionString: connectionString,
+        });
+
+        pool1.query(Update_event, [event.date, event.location, event.partySupplier, event.caterer, event.guests, event.owner, event.id, ],  (err, response) => {
+
+            if(err){
+                pool1.end();
+                return res.send({
+                    errorType: 'InternalError',
+                    message: err,
+                });
+            }
+            pool1.end();
+
+            return res.send({
+                message: 'sucess',
+            });
+        });
+
+        pool.end();
     });
 });
 
