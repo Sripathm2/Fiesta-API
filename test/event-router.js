0011a4407e64e375f8dce572b2442d61ac6396b9
Sripath Mishra <mishra60@purdue.edu>
2019-03-06 21:50:56 +0000

    why

--- test/event-router.js
+++ test/event-router.js
@@ -52,7 +52,7 @@ describe('event-router', function() {
         it('it should succeed with correct fields ', done => {
 
             let postdata = {
-                task: '{atask,second}',
+                task: '{atask}',
                 id: 'ef0f5596-4049-11e9-b210-d663bd873d93',
             };
 
