d52441820d0279efe4629648d5a0f0e345cdbead
Siddharth Dhar <38643819+dharsiddharth7@users.noreply.github.com>
2019-03-08 01:54:54 -0500

    Sid

--- Setup.js
+++ Setup.js
@@ -25,11 +25,21 @@ const designs = new Pool({
     connectionString: connectionString,
 });
 
+const wishlist = new Pool({
+    connectionString: connectionString,
+});
+
+const questionanswer = new Pool({
+    connectionString: connectionString,
+});
+
 const create_user_table = 'CREATE TABLE Users (userName VARCHAR(32) PRIMARY KEY, password text not null, email text not null, securityQuestion text not null, securityAnswer text not null, name text not null, notification text not null)';
 const insert_user = 'INSERT INTO Users (userName, password, email , securityQuestion, securityAnswer, name, notification) VALUES (\'testUsername\',\'$2b$10$PhtMAduAs2i0wI/Uvs6DIepGMjz2JjooKNoDZ1dbYMweuWHGbleQK\', \'test@test.com\', \'what my name?\', \'test answer\', \'test test\', \'  \')';
 const create_feedback_table = 'CREATE TABLE Feedback (feedbackText text)';
 const create_events_table = 'CREATE TABLE Events (id SERIAL PRIMARY KEY, owner VARCHAR(32) not null, date TIMESTAMPTZ not null, location real[2] not null, partySupplier text not null, caterer text not null, guests text[])';
 const create_designs_table = 'CREATE TABLE Designs (id integer PRIMARY KEY, designString text)';
+const create_wishlist_table = 'CREATE TABLE Wishlist (id SERIAL PRIMARY KEY, userName VARCHAR(32) not null,item text not null)';
+const create_ques_ans_table = 'CREATE TABLE Questionanswer (event_id text, questionUserName text, question text, questionID text, answerUsername text, answer text';
 
 function create_user_table_function() {
     pool.query(create_user_table, (err, res) => {
@@ -40,6 +50,18 @@ function create_user_table_function() {
         });
 
     });
+}
+
+function create_question_answer_table_function() {
+    questionanswer.query(create_ques_ans_table, (err, res) => {
+        questionanswer.end();
+    });
+}
+
+function create_wishlist_table_function() {
+    wishlist.query(create_wishlist_table, (err, res) => {
+        wishlist.end();
+    })
 }
 
 function create_feedback_table_function() {
@@ -60,7 +82,9 @@ function create_designs_table_function() {
     });
 }
 
+create_question_answer_table_function();
 create_user_table_function();
 create_feedback_table_function();
 create_events_table_function();
 create_designs_table_function();
+create_wishlist_table_function();
