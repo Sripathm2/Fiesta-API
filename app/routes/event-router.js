95d51c081d8cda73653b6a5a39e0662b95a2073a
Sripath Mishra <mishra60@purdue.edu>
2019-03-27 15:54:48 +0000

    same .

--- app/routes/event-router.js
+++ app/routes/event-router.js
@@ -4,6 +4,7 @@ const { Pool, } = require('pg');
 let nodemailer = require('nodemailer');
 let fs = require('fs');
 const uuidv1 = require('uuid/v1');
+let jwt = require('jsonwebtoken');
 const connectionString = process.env.DB_URL;
 const Insert_event = 'INSERT into Events (id, owner, name, description, date,' +
                        'imageLink, location, partySupplier, caterer,' +
